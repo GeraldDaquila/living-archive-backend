04292dd8506cd752b48656f04ffcb37c0ab0ff59
GeraldDaquila <geralddaquila@yahoo.ca>
2026-08-30 13:35:10 +0800

    Refactor environment variable handling and imports

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import re
 
-# Suppress Hugging Face unauthenticated warnings & tokenizer parallelism notices
+# Suppress Hugging Face Hub unauthenticated download warning & tokenizer notices
+os.environ["HF_HUB_DISABLE_IMPLICIT_TOKEN_WARNING"] = "1"
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
